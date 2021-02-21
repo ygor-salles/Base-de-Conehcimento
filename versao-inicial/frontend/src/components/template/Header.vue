5c2c6ba0e69de1abfb1e00b691978e9e9ab6a2f2
unknown <ygorsallesaniceto@gmail.com>
2021-02-21 18:02:46 -0300

    Inicializando front-end VueJS

---
+++ versao-inicial/frontend/src/components/template/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header class="header">
+        <a class="toogle">
+            <i class="fa fa-lg" :class="icon"></i>
+        </a>
+        <h1 class="title">{{ title }}</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        hideToggle: Boolean
+    },
+    computed: {
+        icon() {
+            return "fa-angle-left"
+        }
+    }
+}
+</script>
+
+<style>
+    .header {
+        grid-area: header;
+        background: linear-gradient(to right, #1e469a, #49a7c1);
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
