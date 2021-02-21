d0dbd60b24e672e8a1245ef978a527090d071e5a
unknown <ygorsallesaniceto@gmail.com>
2021-02-21 15:28:04 -0300

    Adicionando coluna deletedAt - Soft Delete

--- versao-inicial/backend/config/routes.js
+++ versao-inicial/backend/config/routes.js
@@ -14,11 +14,13 @@ module.exports = app => {
         .all(app.config.passaport.authenticate())
         .put(admin(app.api.user.save))
         .get(admin(app.api.user.getById))
+        .delete(admin(app.api.user.remove))
 
     app.route('/categories')
         .all(app.config.passaport.authenticate())
         .get(admin(app.api.category.get))
         .post(admin(app.api.category.save))
+        .unlock(admin(app.api.user.ativar)) 
 
     app.route('/categories/tree')
         .all(app.config.passaport.authenticate())
@@ -44,4 +46,8 @@ module.exports = app => {
     app.route('/categories/:id/articles')
         .all(app.config.passaport.authenticate())
         .get(app.api.article.getByCategory)
+
+    app.route('/stats')
+        .all(app.config.passaport.authenticate())
+        .get(app.api.stat.get)
 }
